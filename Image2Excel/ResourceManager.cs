da713b8ab2c374a14b5dfb2e7639821bbcd5f405
Hồ Nguyên Minh <honguyenminh@outlook.com>
2022-02-02 23:33:08 +0700

    Make ResourceManager static

--- Image2Excel/ResourceManager.cs
+++ Image2Excel/ResourceManager.cs
@@ -3,29 +3,40 @@ using System.Resources;
 
 namespace Image2Excel;
 
-internal class ResourceManager
+internal static class ResourceManager
 {
     private const string ResPathPrefix = "Image2Excel.Resource.";
-    private readonly Assembly _assembly;
+    private static readonly Assembly s_assembly = typeof(Program).Assembly;
 
-    public ResourceManager()
+    /// <summary>
+    /// Get the stream to an embedded resource inside the assembly
+    /// </summary>
+    /// <param name="resourcePath">Path to resource (i.e. OuterFolder.InnerFolder.FileName)</param>
+    /// <returns>Stream to the resource found</returns>
+    /// <exception cref="MissingManifestResourceException">Cannot find resource with given path</exception>
+    public static Stream GetResourceStream(string resourcePath)
     {
-        _assembly = Assembly.GetExecutingAssembly();
-    }
-
-    public Stream GetResourceStream(string resourcePath)
-    {
-        return _assembly.GetManifestResourceStream(ResPathPrefix + resourcePath)
+        return s_assembly.GetManifestResourceStream(ResPathPrefix + resourcePath)
             ?? throw new MissingManifestResourceException("Cannot find resource " + resourcePath);
     }
 
-    public string GetEmbeddedResourceContent(string resourcePath)
+    /// <summary>
+    /// Get the content of an embedded resource inside the assembly as a string
+    /// </summary>
+    /// <param name="resourcePath">Path to resource (i.e. OuterFolder.InnerFolder.FileName)</param>
+    /// <returns>The content of the resource found as a string</returns>
+    /// <exception cref="MissingManifestResourceException">Cannot find resource with given path</exception>
+    public static string GetResourceContent(string resourcePath)
     {
-        using var stream = _assembly.GetManifestResourceStream(ResPathPrefix + resourcePath);
-        if (stream is null)
-            throw new MissingManifestResourceException("Cannot find resource " + resourcePath);
+        using var stream = GetResourceStream(resourcePath);
         using StreamReader source = new(stream);
-        string fileContent = source.ReadToEnd();
-        return fileContent;
+        return source.ReadToEnd();
+    }
+
+    public static void WriteResourceToFile(string resourcePath, string filePath)
+    {
+        using var resourceStream = GetResourceStream(resourcePath);
+        var fileStream = File.OpenWrite(filePath);
+        resourceStream.CopyTo(fileStream);
     }
 }
