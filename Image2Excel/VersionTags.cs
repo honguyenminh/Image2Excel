0ae305ed258c1d6f3e585140a7ccb014e6d35fcf
Hồ Nguyên Minh <honguyenminh@outlook.com>
2022-02-07 20:28:52 +0700

    Bump version 0.2

--- Image2Excel/VersionTags.cs
+++ Image2Excel/VersionTags.cs
@@ -4,6 +4,6 @@ public static class VersionTags
 {
     public const bool IsPreRelease = true;
     public const int Major = 0;
-    public const int Minor = 1;
+    public const int Minor = 2;
     public static readonly string Version = $"v{Major}.{Minor}" + (IsPreRelease ? "pre" : "");
 }
